ad68dddf966f2fd10d56ef54604d052b54d810fa
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 17:53:59 -0600

    Add Note Table View Cell

---
+++ Swamper/Views/NotesCell.swift
@@ -0,0 +1,45 @@
+//
+//  NotesTableViewCell.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/30/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+
+class NotesCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    var note:Note?{
+        didSet{
+            if let note = note{
+                titleLabel.text = note.title
+                contentLabel.text = note.content
+                dateLabel.text = nil
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        titleLabel.text = nil
+        contentLabel.text = nil
+        dateLabel.text = nil
+        
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
