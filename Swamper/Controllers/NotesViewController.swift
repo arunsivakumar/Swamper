3b1a5b862254c8906e64bfa3c32af89b819f769f
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 17:53:35 -0600

    Add Notes VC

---
+++ Swamper/Controllers/NotesViewController.swift
@@ -0,0 +1,58 @@
+//
+//  NotesViewController.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/30/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class NotesViewVontroller: UIViewController{
+    
+    var notes:Results<Note>!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+    }
+    
+    func loadData(){
+        notes = RealmHelper.fetchNotes()
+    }
+}
+
+//MARK:- Table View Data Source
+
+extension NotesViewVontroller: UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "NotesCell", for: indexPath)  as! NotesCell
+        cell.note = notes[indexPath.row]
+        return cell
+    }
+    
+}
+
+//MARK:- Table View Delegate
+
+extension NotesViewVontroller: UITableViewDelegate{
+    
+
+    
+}
+
+//MARK:- Navigation
+extension NotesViewVontroller{
+    
+    
+}
