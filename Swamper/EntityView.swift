a2a362bfb3f7f30b78bfef0d74430e42c999cefa
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 02:41:55 -0600

    Initialize project with components

---
+++ Swamper/EntityView.swift
@@ -0,0 +1,33 @@
+//
+//  EntityButton.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/29/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+
+class EntityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 2.0
+//        self.clipsToBounds = true
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 1
+        self.layer.masksToBounds = false
+    }
+
+}
---
+++ Swamper/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/29/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var notesCountLabel: UILabel!
+    @IBOutlet weak var websitesCountLabel: UILabel!
+    @IBOutlet weak var cardsCountLabel: UILabel!
+    @IBOutlet weak var photosCountLabel: UILabel!
+    @IBOutlet weak var extraCountLabel: UILabel!
+    @IBOutlet weak var freeCountLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+ 
+}
+
