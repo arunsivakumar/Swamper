55a391da18d2a2f4c682b7df8bf5f8ea7707ef33
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 17:52:51 -0600

    Add capability functions

--- Swamper/Helpers/RealmHelper.swift
+++ Swamper/Helpers/RealmHelper.swift
@@ -10,6 +10,24 @@ import Foundation
 import RealmSwift
 
 class RealmHelper{
-
-
+    
+    static func addNote(note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.add(note)
+        }
+    }
+    
+    static func deleteNote(note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.delete(note)
+        }
+    }
+    
+    static func fetchNotes()-> Results<Note>{
+        
+        let realm = try! Realm()
+        return realm.objects(Note.self).sorted(byKeyPath: "updatedAt", ascending: false)
+    }
 }
