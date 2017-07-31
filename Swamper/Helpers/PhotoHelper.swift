d3ddf986ce1b6f6c447f781bde983eaeb12d7024
Arun Sivakumar <arunsivakumar@live.com>
2017-07-31 02:47:15 -0600

    Fix bug on PhotoHelper

--- Swamper/Helpers/PhotoHelper.swift
+++ Swamper/Helpers/PhotoHelper.swift
@@ -14,7 +14,7 @@ class PhotoHelper : NSObject {
     
     private struct Message{
         
-        static let AlertTitle         = "Where do you want to select a photo from"
+        static let AlertTitle         = "Select your source..."
         static let AlertCancelTitle   = "Cancel"
         static let PhotosActionTitle  = "Photos"
         static let CameraActionTitle  = "Camera"
@@ -38,29 +38,42 @@ class PhotoHelper : NSObject {
     
     private func showPhotoSourceOptions() {
         
+//        let alertController = UIAlertController(title: nil, message: Message.AlertTitle, preferredStyle: .actionSheet)
+//        let cancelAction = UIAlertAction(title: Message.AlertCancelTitle, style: .destructive, handler: nil)
+//        alertController.addAction(cancelAction)
+//        
+//        // 1. Library
+//        let photoLibraryAction = UIAlertAction(title:Message.PhotosActionTitle , style: .default) { (action) in
+//            
+//            self.showImagePickerController(sourceType: .photoLibrary)
+//        }
+//        
+//        alertController.addAction(photoLibraryAction)
+//        
+//        // 2. Rear camera
+//        if (UIImagePickerController.isCameraDeviceAvailable(.rear)) {
+//            let cameraAction = UIAlertAction(title: Message.CameraActionTitle , style: .default) { (action) in
+//                self.showImagePickerController(sourceType: .camera)
+//            }
+//        alertController.addAction(cameraAction)
+//        }
+        
+   
+        
         let alertController = UIAlertController(title: nil, message: Message.AlertTitle, preferredStyle: .actionSheet)
-        let cancelAction = UIAlertAction(title: Message.AlertCancelTitle, style: .destructive, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: Message.AlertCancelTitle, style: .cancel) { action in
+            
+        }
         alertController.addAction(cancelAction)
         
-        // 1. Library
-        let photoLibraryAction = UIAlertAction(title:Message.PhotosActionTitle , style: .default) { (action) in
-            
-            self.showImagePickerController(sourceType: .photoLibrary)
+        let photosAction = UIAlertAction(title: Message.PhotosActionTitle, style: .default) { action in
+           self.showImagePickerController(sourceType: .photoLibrary)
         }
-        
-        alertController.addAction(photoLibraryAction)
-        
-        // 2. Rear camera
-        if (UIImagePickerController.isCameraDeviceAvailable(.rear)) {
-            let cameraAction = UIAlertAction(title: Message.CameraActionTitle , style: .default) { (action) in
-                self.showImagePickerController(sourceType: .camera)
-            }
-            alertController.addAction(cameraAction)
-        }
+        alertController.addAction(photosAction)
         
         // Show ActionSheet
         viewController.present(alertController, animated: true, completion: nil)
-        
     }
     
     private func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
@@ -79,15 +92,15 @@ class PhotoHelper : NSObject {
 extension PhotoHelper: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // User picked an image
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         viewController.dismiss(animated: false, completion: nil)
         callback(image)
     }
     
-    // User cancelled the selection process
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         viewController.dismiss(animated: true, completion: nil)
         callback(nil)
     }
-    
 }
