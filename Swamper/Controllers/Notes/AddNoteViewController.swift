c5b627170d4d9b09d6615b1614be6de146626c1f
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 22:17:42 -0600

    Add Create note

--- Swamper/Controllers/Notes/AddNoteViewController.swift
+++ Swamper/Controllers/Notes/AddNoteViewController.swift
@@ -14,7 +14,57 @@ import RealmSwift
 
 class AddNoteViewController: UIViewController{
 
+    
+    var note:Note?
 
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+        
+    }
+    
+    func loadData(){
+        if let note = note{
+            titleTextField.text = note.title
+            contentTextView.text = note.content
+        }
+    }
+    
+    func saveChanges(){
+        
+        
+        if let note = note{  // 1.Update
+            
+        }else{  // 2. Create Note
+            let note = Note()
+            note.title = titleTextField.text ?? ""
+            note.content = contentTextView.text ?? ""
+            note.updatedAt = Date()
+            
+            RealmHelper.addNote(note: note)
+        }
+        
+    }
+}
 
-
+//MARK:- Navigation
+extension AddNoteViewController{
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "save"?:
+            saveChanges()
+         
+            let vc =
+                segue.destination as! NotesViewController
+            vc.loadData()
+            
+        case "addNote"?: break
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
 }
