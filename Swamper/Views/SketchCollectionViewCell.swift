365d8a615e4282c2181cccb65575c344bf99354f
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 20:44:23 -0600

    Add SketchcollectionviewCell

---
+++ Swamper/Views/SketchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SketchCollectionViewCell.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/30/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+
+class SketchCollectionViewCell: UICollectionViewCell {
+    
+}
