700eb88a9aed0c166925f01426d64a12ab2aabd3
Arun Sivakumar <arunsivakumar@live.com>
2017-07-31 02:47:56 -0600

    Change photo data source

--- Swamper/DataSource/PhotosDataSource.swift
+++ Swamper/DataSource/PhotosDataSource.swift
@@ -18,17 +18,30 @@ protocol DataSource:class{
 
 class PhotosDataSource:NSObject,UITableViewDataSource{
     
-    var photos:Results<Photo>!
-    
-    weak var delegate:DataSource?
+    var photoStore: PhotoStore!
+    var imageStore:ImageStore!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photos.count
+        return photoStore.photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoTableViewCell", for: indexPath)  as! PhotoTableViewCell
-        cell.photo = photos[indexPath.row]
+        cell.photo = photoStore.photos[indexPath.row]
+        cell.imageStore = self.imageStore
         return cell
     }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let photo = photoStore.photos[indexPath.row]
+            self.photoStore.removePhoto(photo)
+            self.imageStore.deleteImage(for: photo.photoKey)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    
 }
