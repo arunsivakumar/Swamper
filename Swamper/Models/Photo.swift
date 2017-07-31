4fda274d8d74324d479461de07542d311d20c41f
Arun Sivakumar <arunsivakumar@live.com>
2017-07-31 02:48:29 -0600

    Change Photo for archiving

--- Swamper/Models/Photo.swift
+++ Swamper/Models/Photo.swift
@@ -5,15 +5,34 @@
 //  Created by Lakshmi on 7/30/17.
 //  Copyright © 2017 com.arunsivakumar. All rights reserved.
 //
-
 import Foundation
+import UIKit
+
+class Photo: NSObject, NSCoding  {
 
 
-import Foundation
-import RealmSwift
+//    let image:UIImage
+    let updatedAt:Date
+    let photoKey: String
+    
+    
+    override init(){
+        
+        self.photoKey = UUID().uuidString
+        self.updatedAt = Date()
+//        self.image = nil
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        updatedAt = aDecoder.decodeObject(forKey: "updatedAt") as! Date
+        photoKey = aDecoder.decodeObject(forKey: "photoKey") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(updatedAt, forKey: "updatedAt")
+        aCoder.encode(photoKey, forKey: "photoKey")
+    }
 
-class Photo: Object {
-
-//    var image = UIImage()
-    dynamic var updatedAt = NSDate()
+    
 }
