c9850ce925a1e8d8d1d4dcf0d2b13a45ef27b606
Arun Sivakumar <arunsivakumar@live.com>
2017-07-30 18:19:44 -0600

    Add PhotoCell

---
+++ Swamper/Views/PhotoCell.swift
@@ -0,0 +1,41 @@
+//
+//  PhotoCell.swift
+//  Swamper
+//
+//  Created by Lakshmi on 7/30/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    var photo:Photo?{
+        didSet{
+            if let photo = photo{
+                imageView.image = photo.image
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+//        imageView.image = nil
+        
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
